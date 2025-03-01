5a45aee1f607a0e3e28e6e679259de258d6351ff
Amanda Siriwardane <150885291+amandasiriwardane@users.noreply.github.com>
2025-03-01 21:10:53 +0530

    aa

--- Dashboard/SafePlus-FrontEnd-Dashboard/src/App.jsx
+++ Dashboard/SafePlus-FrontEnd-Dashboard/src/App.jsx
@@ -1,10 +1,12 @@
 // src/App.js
 import React from 'react';
 import IotCoreComponent from './IotCoreComponent';
-
+import Header from './components/Header/Header';
+import './index.css'
 const App = () => {
   return (
-    <div>
+    <div className='container'>
+      <Header />
       <IotCoreComponent />
     </div>
   );
---
+++ dashboard/SafePlus-FrontEnd-Dashboard/src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Header.css'
+import logo from '../../assets/logo.webp'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <img src={logo} alt='' className='logo'/>
+      <ul>
+        <li>Live Data</li>
+        <li>Worker Details</li>
+        <li>Reports</li>
+      </ul>
+      <img src='' alt='' className='setting'/>
+
+    </div>
+  )
+}
+
+export default Header
