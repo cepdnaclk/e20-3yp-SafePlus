bd5a8b50df7625931c115900cfda1a2cc1063d3a
Amanda Siriwardane <150885291+amandasiriwardane@users.noreply.github.com>
2025-03-02 18:14:36 +0530

    path

--- Dashboard/SafePlus-FrontEnd-Dashboard/src/App.jsx
+++ Dashboard/SafePlus-FrontEnd-Dashboard/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route,Navigate } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
 import LoginPage from './pages/LoginPage';
 import Home from './pages/Home';
@@ -8,6 +8,7 @@ const App = () => {
     <ChakraProvider>
       <Router>
         <Routes>
+         <Route path="/" element={<Navigate to="/login" />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/signup" element={<SignupPage />} />
 
