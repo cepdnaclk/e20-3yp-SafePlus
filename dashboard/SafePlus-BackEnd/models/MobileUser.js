1ea80eb991d785c20129ce605c7bda27206a829d
samadhisakunika <150885396+samadhisakunika@users.noreply.github.com>
2025-05-26 16:11:14 +0530

    backend

--- dashboard/SafePlus-BackEnd/models/MobileUser.js
+++ dashboard/SafePlus-BackEnd/models/MobileUser.js
@@ -8,16 +8,3 @@ const mobileUserSchema = new mongoose.Schema({
 
 module.exports = mongoose.model("MobileUser", mobileUserSchema);
 
-const helmetDataSchema = new mongoose.Schema({
-  temperature: { type: Number, required: true },
-  humidity: { type: Number, required: true },
-  acc_magnitude: { type: Number, required: true },
-  gyro_magnitude: { type: Number, required: true },
-  heart_rate: { type: Number, required: true },
-  location: { lat: Number, lng: Number },
-  gasvalues: { type: Number, required: true },
-  button: { type: Boolean, required: true },
-  impact: { type: String, required: true },
-  timestamp: { type: Date, default: Date.now }  // ✅ Add this
-});
-module.exports = mongoose.model("HelmetData", helmetDataSchema);
