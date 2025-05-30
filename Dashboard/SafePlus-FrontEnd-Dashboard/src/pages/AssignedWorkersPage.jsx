4daea4a372f4db5e425c0c95832f7e7cd2a5fa95
Amanda Siriwardane <150885291+amandasiriwardane@users.noreply.github.com>
2025-05-30 21:47:05 +0530

    advancedsecurity

--- Dashboard/SafePlus-FrontEnd-Dashboard/src/pages/AssignedWorkersPage.jsx
+++ Dashboard/SafePlus-FrontEnd-Dashboard/src/pages/AssignedWorkersPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import  { useEffect, useState } from "react";
 import WorkerCard from "../components/WorkerCard/WorkerCard";
 import axios from "axios";
 
