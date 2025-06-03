8ab31f0b1f185215cc1aa06cd59cd6838001e8af
piyusam0133 <e20346@eng.pdn.ac.lk>
2025-06-03 19:02:54 +0530

    workerdetails page fix

--- dashboard/SafePlus-FrontEnd-Dashboard/src/pages/WorkerDetails.jsx
+++ dashboard/SafePlus-FrontEnd-Dashboard/src/pages/WorkerDetails.jsx
@@ -6,9 +6,6 @@ import '../styles/WorkerDetails.css';
 import AssignHelmetForm from '../components/AssignHelmet/AssignHelmetForm';
 import ConfirmDeleteForm from '../components/ConfirmDelete/ConfirmDeleteForm';
 import { toast } from 'react-hot-toast'; 
-const { sendNotification } = useNotifications();
-
-sendNotification({ id, message, onClick });
 
 const WorkerDetails = () => {
   const [workers, setWorkers] = useState([]);
