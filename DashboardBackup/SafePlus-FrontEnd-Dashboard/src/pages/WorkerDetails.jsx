940d5f03489e9d965b5dd03b94714baae45ccee9
Amanda Siriwardane <150885291+amandasiriwardane@users.noreply.github.com>
2025-07-07 00:15:09 +0530

    Update WorkerDetails.jsx

--- DashboardBackup/SafePlus-FrontEnd-Dashboard/src/pages/WorkerDetails.jsx
+++ DashboardBackup/SafePlus-FrontEnd-Dashboard/src/pages/WorkerDetails.jsx
@@ -8,7 +8,7 @@ import ConfirmDeleteForm from '../components/ConfirmDelete/ConfirmDeleteForm';
 import { toast } from 'react-hot-toast';
 
 // Use environment variable for API base URL
-const API_URL = process.env.REACT_APP_API_URL;
+const API_URL = import.meta.env.VITE_API_URL;
 
 const WorkerDetails = () => {
   const [workers, setWorkers] = useState([]);
